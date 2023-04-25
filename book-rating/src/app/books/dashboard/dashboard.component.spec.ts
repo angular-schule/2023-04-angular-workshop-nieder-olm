7b1313febb5a3210fb2e0e4dcf9e4ea576632eb8
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-04-25 12:13:28 +0200

    Standalone: Shallow Component Tests

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -2,6 +2,17 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DashboardComponent } from './dashboard.component';
 import { Book } from '../shared/book';
+import { Component, Input } from '@angular/core';
+import { BookComponent } from '../book/book.component';
+
+@Component({
+  selector: 'br-book',
+  template: '😃',
+  standalone: true
+})
+export class DummyBookComponent {
+  @Input() book?: Book;
+}
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -10,7 +21,11 @@ describe('DashboardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
     imports: [DashboardComponent]
-})
+    })
+    .overrideComponent(DashboardComponent, {
+      remove: { imports: [BookComponent] },
+      add: { imports: [DummyBookComponent] }
+    })
     .compileComponents();
 
     fixture = TestBed.createComponent(DashboardComponent);
