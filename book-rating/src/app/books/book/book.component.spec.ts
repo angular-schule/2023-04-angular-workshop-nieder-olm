2fd6998eda847282f76c8c03a10e9fd34c881152
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-04-26 17:30:27 +0200

    testing: OnPush components are harder to test!
    see https://github.com/angular/angular/issues/12313

--- book-rating/src/app/books/book/book.component.spec.ts
+++ book-rating/src/app/books/book/book.component.spec.ts
@@ -1,23 +1,38 @@
+import { Component, Input } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Book } from '../shared/book';
 import { BookComponent } from './book.component';
 
+// OnPush components are harder to test
+// see https://github.com/angular/angular/issues/12313
+@Component({
+  template: '<br-book [book]="book"></br-book>',
+  standalone: true,
+  imports: [BookComponent]
+})
+export class BookWrapperComponent {
+  @Input() book?: Book;
+}
+
 describe('BookComponent', () => {
-  let component: BookComponent;
-  let fixture: ComponentFixture<BookComponent>;
+  let component: BookWrapperComponent;
+  let fixture: ComponentFixture<BookWrapperComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [BookComponent]
-})
-    .compileComponents();
+      imports: [BookWrapperComponent],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(BookComponent);
+    fixture = TestBed.createComponent(BookWrapperComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should disable the rate down button when rating is smaller 2', () => {
+    component.book = { isbn: '323', title: 'Test', description: 'rest', rating: 1 };
+    fixture.detectChanges();
+    const button = fixture.nativeElement.querySelectorAll('button')[0];
+
+    expect(button.disabled).toBe(true);
   });
 });
