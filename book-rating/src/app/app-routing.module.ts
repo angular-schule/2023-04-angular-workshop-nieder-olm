4a5d561810a9a11bc5bcadeba4d3cfa807b9e6fc
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-04-27 11:36:45 +0200

    Lazy Loading mit Standalone

--- book-rating/src/app/app-routing.module.ts
+++ book-rating/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'books' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    // preloadingStrategy: PreloadAllModules
+    // enableTracing: true
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- book-rating/src/app/books/books-routing.module.ts
+++ book-rating/src/app/books/books-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { BookDetailsComponent } from './book-details/book-details.component';
 
 const routes: Routes = [
   { path: 'books', component: DashboardComponent },
-  { path: 'books/:isbn', component: BookDetailsComponent }
+  // { path: 'books/:isbn', component: BookDetailsComponent }
+
+  // Lazy Loading mit Standalone – voll wenig aufwand! =)
+  { path: 'books/:isbn', loadComponent: () => import('./book-details/book-details.component')
+}
+
 ];
 
 @NgModule({
