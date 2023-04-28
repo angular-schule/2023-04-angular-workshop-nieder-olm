62f2ca04fed031ab667eca2d2f2dd8c742ffd095
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2023-04-28 14:55:33 +0200

    ngrx: selectors

--- book-rating/src/app/books/dashboard/dashboard.component.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.ts
@@ -1,49 +1,52 @@
-import { NgFor } from '@angular/common';
-import { Component } from '@angular/core';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { Store } from '@ngrx/store';
 
 import { BookCreateComponent } from '../book-create/book-create.component';
 import { BookComponent } from '../book/book.component';
 import { Book } from '../shared/book';
 import { BookRatingService } from '../shared/book-rating.service';
 import { BooksService } from '../shared/http';
+import { selectBooks, selectBooksLoading } from '../store/book.selectors';
 
 @Component({
   selector: 'br-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
   standalone: true,
-  imports: [NgFor, BookComponent, BookCreateComponent],
+  imports: [NgFor, BookComponent, BookCreateComponent, AsyncPipe, NgIf],
   // changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DashboardComponent {
 
-  books: Book[] = [];
+  books$ = inject(Store).select(selectBooks);
+  loading$ = inject(Store).select(selectBooksLoading);
 
-  constructor(private br: BookRatingService, private bs: BooksService) {
-    this.bs.booksGet().subscribe(books => this.books = books);
-  }
+  // constructor(private br: BookRatingService, private bs: BooksService) {
+    // this.bs.booksGet().subscribe(books => this.books = books);
+  // }
 
   doRateUp(book: Book): void {
-    const ratedBook = this.br.rateUp(book);
-    // const ratedBook = {
-    //   ...book,
-    //   rating: book.rating < 5 ? book.rating + 1 : 1
-    // }
-    this.updateAndSort(ratedBook);
+    // const ratedBook = this.br.rateUp(book);
+    // // const ratedBook = {
+    // //   ...book,
+    // //   rating: book.rating < 5 ? book.rating + 1 : 1
+    // // }
+    // this.updateAndSort(ratedBook);
   }
 
   doRateDown(book: Book): void {
-    const ratedBook = this.br.rateDown(book);
-    this.updateAndSort(ratedBook);
+    // const ratedBook = this.br.rateDown(book);
+    // this.updateAndSort(ratedBook);
   }
 
   updateAndSort(ratedBook: Book) {
-    this.books = this.books
-      .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
-      .sort((a, b) => b.rating - a.rating);
+    // this.books = this.books
+    //   .map(b => b.isbn === ratedBook.isbn ? ratedBook : b)
+    //   .sort((a, b) => b.rating - a.rating);
   }
 
   addBook(newBook: Book): void {
-    this.books = [...this.books, newBook]
+    // this.books = [...this.books, newBook]
   }
 }
